1ff58be37277fd779979637154f2ed55d6c8dd73
Raider472 <gabrielnae1@gmail.com>
2023-05-12 17:22:16 +0200

    Class pokemon

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -14,7 +14,7 @@ class TestWidget extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.light),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.dark,
       home: const Home(),
     );
   }
@@ -30,17 +30,87 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Yepa")),
       body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.3),
-              borderRadius: BorderRadius.circular(80)),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 12,
-          ),
-          child: const Text('Hello World'),
+        child: Column(
+          children: const [
+            TheAmazingRow(
+              label: "Yepa",
+              icon: Icons.snowboarding,
+            ),
+            SizedBox(height: 16),
+            TheAmazingRow(
+              label: "Yopo",
+              icon: Icons.cloud,
+            ),
+            SizedBox(height: 16),
+            TheAmazingRow(
+              label: "Yolo",
+              icon: Icons.sunny,
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class TheAmazingRow extends StatelessWidget {
+  const TheAmazingRow({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(80)),
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 12,
+      ),
+      child: Row(
+        children: [
+          Icon(icon),
+          /*const Spacer(
+            flex: 2,
+          ),*/
+          SizedBox(
+            width: 16,
+          ),
+          Text(label),
+          const Spacer(
+            flex: 2,
+          ),
+          /*SizedBox(
+            width: 160,
+          ),*/
+          IconButton(
+            icon: const Icon(Icons.add_a_photo),
+            onPressed: () => print(label),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Pokemon {
+  IconData image_;
+  String nom_;
+
+  Pokemon({this.image_, this.nom_});
+
+  String get nom {
+    return nom_ ?? "Pas de nom";
+  }
+
+  IconData get image {
+    return image_;
   }
 }
